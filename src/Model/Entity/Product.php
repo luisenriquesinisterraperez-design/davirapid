16b3cb9c30ba9f5cf98e7e279d0b1b60e9796f98
Alexander Caicedo <a.caicedo.dev@gmail.com>
2026-05-02 18:21:29 -0500

    feat(products): add Product entity

---
+++ src/Model/Entity/Product.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Model\Entity;
+
+use Cake\ORM\Entity;
+
+class Product extends Entity
+{
+    protected array $_accessible = [
+        'code' => true,
+        'name' => true,
+        'description' => true,
+        'price' => true,
+        'image_path' => true,
+        'is_active' => true,
+    ];
+
+    public function getImageUrl(): string
+    {
+        if (empty($this->image_path)) {
+            return '/img/product-placeholder.svg';
+        }
+        return '/' . ltrim((string)$this->image_path, '/');
+    }
+
+    public function getFormattedPrice(): string
+    {
+        return '$' . number_format((int)$this->price, 0, ',', '.');
+    }
+}
